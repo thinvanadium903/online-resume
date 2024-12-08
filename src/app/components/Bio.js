b709a93677a2d81eb264f5d985c2f3eaab2bc9e9
thinvanadium903 <ppb93083@uga.edu>
2024-12-07 19:15:56 -0500

    more formatting work, good stopping point

--- src/app/components/Bio.js
+++ src/app/components/Bio.js
@@ -1,12 +1,26 @@
-import Headshot from "./Headshot";
-import Textblock from "./TextBlock";
 import '../styles/Bio.css'
 
 function Bio() {
     return (
         <div id='bio'>
-            <Headshot></Headshot>
-            <Textblock></Textblock>
+            <div id='headshot'></div>
+            <section>
+                <p>
+                    Hello! You've landed on my online portfolio. It contains links to my resumé, 
+                    cover letter, LinkedIn profile, academic transcript, my contact information, 
+                    and some samples of my work.
+                </p>
+                <p>
+                    I'm currently a computer science student in my senior year at the University of 
+                    Georgia. This site was made in December 2024 as my final portfolio project for my Career and 
+                    Life Planning Class. Updates will be made in the future as needed.
+                </p>
+                <p>
+                    To Professor Dr. Nikki Hawkins, thank you so much for an amazing semester! You 
+                    were very helpful and kind throughout the semester and I wish you the best.
+                </p>
+                <div id='signature'></div>
+            </section>
         </div>
         
     )
